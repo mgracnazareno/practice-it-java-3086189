58e4746125ad6f81e80d433f3a65f9a882885a49
Mary Nazareno <47845955+mgracnazareno@users.noreply.github.com>
2023-08-20 03:17:32 +0000

    For loop done

--- _03_04/ForLoops.java
+++ _03_04/ForLoops.java
@@ -4,11 +4,18 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
-
+    for (int i = 1; i <= 5; i++) {
+      System.out.println("I love for loops");
+    }
     // Write a for loop that prints out the numbers 1 to 10
-
+    for (int j = 1; j <= 10; ++j) {
+      System.out.print(j + " ");
+    }
     // Write a for loop that prints out the numbers 10 to 1
-    
+    System.out.println();
+    for (int k = 10; k >= 1; k--) {
+      System.out.print(k + " ");
+    }
 
   }
 
